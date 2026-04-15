11ee9b2e10925e08880c30a25110f397c8d76790
Elizabeth Anderson <elizabethanderson4250@users.noreply.github.com>
2026-04-16 07:02:06 +0800

    Add StandardScaler to data package exports for personal experiments

--- kronos/data/__init__.py
+++ kronos/data/__init__.py
@@ -1,12 +1,14 @@
 """Kronos data utilities package."""
 
+# Personal fork note: added StandardScaler import for use in my experiments
 from kronos.data.loader import load_csv, split_train_test, compute_returns
-from kronos.data.preprocessor import MinMaxScaler, build_sequences
+from kronos.data.preprocessor import MinMaxScaler, StandardScaler, build_sequences
 
 __all__ = [
     "load_csv",
     "split_train_test",
     "compute_returns",
     "MinMaxScaler",
+    "StandardScaler",
     "build_sequences",
 ]
